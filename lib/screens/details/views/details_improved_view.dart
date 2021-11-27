daa50cd1cecd961512bfef0baaba97647c80c5b5
BenPrzychodzki <beniamin.przychodzki@gmail.com>
2021-11-28 00:27:55 +0100

    feat: Better details view UI

--- lib/screens/details/views/details_improved_view.dart
+++ lib/screens/details/views/details_improved_view.dart
@@ -1,10 +1,9 @@
 import 'dart:ui';
-
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-
+import '../../capitalize.dart';
 import '../details_bloc.dart';
-import '../details_event.dart';
 import '../details_state.dart';
 
 /// View shown after clicking on a grid view item.
@@ -16,133 +15,167 @@ class DetailsView extends StatelessWidget {
   Widget build(BuildContext context) {
     final double width = MediaQuery.of(context).size.width;
     final double height = MediaQuery.of(context).size.height;
-    return WillPopScope(
-        onWillPop: () {
-          BlocProvider.of<DetailsBloc>(context).add(CloseDetailsPopup());
-          Navigator.pop(context, false);
-          return Future.value(false);
-        },
-        child: Scaffold(
-          backgroundColor: typeColors.first,
-
-          body: _buildDetailsViewBody(width, height),
-        ));
+    return Dismissible(
+      resizeDuration: const Duration(milliseconds: 15),
+      movementDuration: const Duration(milliseconds: 1500),
+      background: Container(
+        decoration: _getBackgroundDecoration(),
+      ),
+      secondaryBackground: Container(
+        color: Colors.white,
+      ),
+      direction: DismissDirection.vertical,
+      key: const Key('key'),
+      onDismissed: (_) => Navigator.of(context).pop(),
+      child: Scaffold(
+        //backgroundColor: typeColors.first,
+        body: Container(
+          decoration: _getBackgroundDecoration(),
+          child: _buildDetailsViewBody(width, height),
+        ),
+      ),
+    );
   }
 
-  BlocBuilder<DetailsBloc, DetailsState?> _buildDetailsViewBody(double appWidth, double appHeight) {
-    return BlocBuilder<DetailsBloc, DetailsState?>(
-      builder: (context, state) {
-        return Stack(
-            children: [
-              Positioned(
-                bottom: 0,
-                child: Container(
-                  width: appWidth,
-                  height: appHeight * 0.6,
-                  decoration: const BoxDecoration(
-                    color: Colors.white,
-                    borderRadius: BorderRadius.only(
-                        topLeft: Radius.circular(20),
-                        topRight: Radius.circular(20)),
-                  ),
+  BlocBuilder<DetailsBloc, DetailsState?> _buildDetailsViewBody(
+      double appWidth, double appHeight) {
+    return BlocBuilder<DetailsBloc, DetailsState?>(builder: (context, state) {
+      return Stack(children: [
+        Positioned(
+          bottom: 0,
+          child: Container(
+            width: appWidth,
+            height: appHeight * 0.6,
+            decoration: const BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(20), topRight: Radius.circular(20)),
+            ),
+            child: Column(
+              children: [
+                SizedBox(
+                  height: appHeight * 0.05,
                 ),
-              ),
-              if (state is DetailsLoadingSuccess) ...[
-                Positioned(
-                    top: appHeight * 0.32,
-                    left: (appWidth / 2) - 50,
-                    child: Hero(tag: "details/${state.pokemon.id}",
-                    child: Image.network(state.pokemon.imageUrl))
-                ),
-                Positioned(
-                    top: appHeight * 0.50,
-                    left: (appWidth / 2) - 50,
-                    child: Text(state.pokemon.name)),
-                Positioned(
-                    top: appHeight * 0.55,
-                    child: Text(state.pokemon.name)),
-                Positioned(
-                    top: appHeight * 0.6,
-                    left: (appWidth / 2) - 50,
-                    child: Text(state.pokemon.name)),
-                Positioned(
-                    top: appHeight * 0.65,
-                    left: (appWidth / 2) - 50,
-                    child: Text(state.pokemon.name)),
-              ]
-
-            ]
-        );
-      }
-      );
-
-
-
-      //   BlocBuilder<DetailsBloc, DetailsState?>(builder: (context, state) {
-      //   if (state is LoadingDetailsState) {
-      //     return const Center(
-      //         child: SpinKitFadingCube(
-      //           color: Colors.red,
-      //         ));
-      //   } else if (state is DetailsLoadingSuccess) {
-      //     return Hero(
-      //       tag: "details/${state.pokemon.id}",
-      //       child: Column(
-      //         mainAxisAlignment: MainAxisAlignment.center,
-      //         crossAxisAlignment: CrossAxisAlignment.center,
-      //         children: [
-      //           SizedBox(
-      //               width: 150,
-      //               height: 150,
-      //               child: Image.network(state.pokemon.imageUrl)),
-      //           const SizedBox(height: 15),
-      //           Text(capitalize(state.pokemon.name),
-      //               style: const TextStyle(fontWeight: FontWeight.bold)),
-      //           const SizedBox(height: 5),
-      //           Text("ID: ${state.pokemon.id}",
-      //               style: const TextStyle(color: Colors.grey)),
-      //           const SizedBox(height: 15),
-      //           Text(_getTypesString(state.pokemon.types)),
-      //           const SizedBox(height: 10),
-      //           Text(
-      //             state.pokemon.description,
-      //             textAlign: TextAlign.center,
-      //             style: const TextStyle(fontStyle: FontStyle.italic),
-      //           ),
-      //           const SizedBox(height: 10),
-      //           Row(
-      //             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-      //             children: [
-      //               Text("Weight: ${state.pokemon.weight / 10} kg"),
-      //               Text("Height: ${state.pokemon.height * 10} cm")
-      //             ],
-      //           ),
-      //         ],
-      //       ),
-      //     );
-      //   } else if (state is DetailsLoadingFailure) {
-      //     return Text(state.error);
-      //   } else {
-      //     return Container();
-      //   }
-      // }),
-
+                if (state is DetailsLoadingSuccess) ...[
+                  Padding(
+                      padding: const EdgeInsets.fromLTRB(20, 0, 0, 20),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: [
+                          SizedBox(
+                              width: appWidth * 0.3,
+                              child: const Text(
+                                "Name: ",
+                                style:
+                                    TextStyle(color: Colors.grey, fontSize: 18),
+                              )),
+                          SizedBox(
+                              width: appWidth * 0.3,
+                              child: Text(capitalize(state.pokemon.name))),
+                        ],
+                      )),
+                  Padding(
+                      padding: const EdgeInsets.fromLTRB(20, 0, 0, 20),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: [
+                          SizedBox(
+                              width: appWidth * 0.3,
+                              child: Text(
+                                state.pokemon.types.length > 1
+                                    ? "Types: "
+                                    : "Type: ",
+                                style: const TextStyle(
+                                    color: Colors.grey, fontSize: 18),
+                              )),
+                          SizedBox(
+                              width: appWidth * 0.3,
+                              child: Text(
+                                state.pokemon.types.length > 1
+                                    ? "${capitalize(state.pokemon.types.first)}, ${capitalize(state.pokemon.types.last)}"
+                                    : capitalize(state.pokemon.types.first),
+                              )),
+                        ],
+                      )),
+                  Padding(
+                      padding: const EdgeInsets.fromLTRB(20, 0, 0, 20),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: [
+                          SizedBox(
+                              width: appWidth * 0.30,
+                              child: const Text(
+                                "Width: ",
+                                style:
+                                    TextStyle(color: Colors.grey, fontSize: 18),
+                              )),
+                          SizedBox(
+                              width: appWidth * 0.30,
+                              child: Text("${state.pokemon.height * 10} cm")),
+                        ],
+                      )),
+                  Padding(
+                      padding: const EdgeInsets.fromLTRB(20, 0, 0, 20),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: [
+                          SizedBox(
+                              width: appWidth * 0.30,
+                              child: const Text(
+                                "Weight: ",
+                                style:
+                                    TextStyle(color: Colors.grey, fontSize: 18),
+                              )),
+                          SizedBox(
+                              width: appWidth * 0.30,
+                              child: Text("${state.pokemon.weight / 10} kg")),
+                        ],
+                      )),
+                  Padding(
+                      padding: const EdgeInsets.fromLTRB(20, 0, 0, 20),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          SizedBox(
+                              width: appWidth * 0.30,
+                              height: appHeight * 0.03,
+                              child: const Text(
+                                "Description: ",
+                                style:
+                                    TextStyle(color: Colors.grey, fontSize: 18),
+                              )),
+                          SizedBox(
+                              width: appWidth * 0.85,
+                              child: Text(
+                                state.pokemon.description,
+                                textAlign: TextAlign.center,
+                                style: const TextStyle(
+                                    fontStyle: FontStyle.italic),
+                              )),
+                        ],
+                      )),
+                ]
+              ],
+            ),
+          ),
+        ),
+        if (state is DetailsLoadingSuccess) ...[
+          Positioned(
+              top: appHeight * 0.325,
+              left: (appWidth / 2) - 50,
+              child: Hero(
+                  tag: "details/${state.pokemon.id}",
+                  child: CachedNetworkImage(
+                    imageUrl: state.pokemon.imageUrl,
+                  ))),
+        ],
+      ]);
+    });
   }
 
-  /// Combine types list to string.
-  String _getTypesString(List<String> types) {
-    String typesString = "";
-    for (String element in types) {
-      typesString += "   ${capitalize(element)}";
-    }
-    return typesString;
-  }
-
-  /// Used for capitalize strings (e.g. pokemon names)
-  String capitalize(String string) {
-    if (string.isEmpty) {
-      return string;
-    }
-    return string[0].toUpperCase() + string.substring(1);
+  BoxDecoration _getBackgroundDecoration() {
+    return BoxDecoration(
+        gradient:
+            LinearGradient(colors: [typeColors.first!, typeColors.last!]));
   }
 }
